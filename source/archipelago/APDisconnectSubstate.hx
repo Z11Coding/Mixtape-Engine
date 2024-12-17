8ac3a361a7b8324fa887a607de40386a898ea562
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2024-12-17 00:39:16 -0500

    pain

---
+++ source/archipelago/APDisconnectSubstate.hx
@@ -0,0 +1,111 @@
+package archipelago;
+
+import ap.Client;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.math.FlxPoint;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import lime.app.Event;
+
+using flixel.util.FlxSpriteUtil;
+
+class APDisconnectSubState extends FlxSubState
+{
+	public var onCancel(default, null) = new Event<Void->Void>();
+	public var onReconnect(default, null) = new Event<Void->Void>();
+
+	private var _ap:Client;
+	private var _seed:String;
+
+	public function new(ap:Client)
+	{
+		super(FlxColor.fromRGBFloat(0, 0, 0, .5));
+		_ap = ap;
+		_seed = _ap.seed;
+
+		_ap._hOnRoomInfo = () ->
+		{
+			if (_seed != _ap.seed)
+			{
+				trace("Seed mismatch; aborting connection");
+				_ap.disconnect_socket();
+				onCancel.dispatch();
+				close();
+			}
+			else
+			{
+				trace("Got room info - sending connect packet");
+
+				#if debug
+				var tags = ["AP", "Testing"];
+				#else
+				var tags = ["AP"];
+				#end
+				ap.ConnectSlot(_ap.slot, null, 0x7, tags, {major: 0, minor: 3, build: 8}); // HACK: this is not retransmitting the password
+			}
+		};
+
+		_ap._hOnSlotRefused = (_) -> onCancel.dispatch();
+
+		_ap._hOnSocketDisconnected = onCancel.dispatch;
+
+		_ap._hOnSlotConnected = (slotData:Dynamic) ->
+		{
+			trace("Connected - returning to game state");
+			ap._hOnRoomInfo = () -> {};
+			ap._hOnSlotRefused = (_) -> {};
+			ap._hOnSocketDisconnected = () -> {};
+			ap._hOnSlotConnected = (_) -> {};
+			onReconnect.dispatch();
+			close();
+		}
+	}
+
+	override function create()
+	{
+		var dcText = new FlxText(0, 0, 0, "Disconnected", 20);
+		dcText.color = FlxColor.WHITE;
+
+		var dcCaption = new FlxText(0, 0, 0, 
+            "You got disconnected from the server.
+            \nWe're trying to get you back on now.
+            \nIf you cancel, you'll be sent back to the connect screen,
+            \nbut your game is saved up to the last song you've beaten.");
+		dcCaption.color = FlxColor.WHITE;
+
+		var cancelButton = new FlxButton(0, 0, "Cancel", () ->
+		{
+			onCancel.dispatch();
+			close();
+		});
+		cancelButton.scrollFactor.set(1, 1);
+
+        var backdrop = new FlxSprite(-11, -11);
+		backdrop.makeGraphic(Math.round(connectingText.width + 22), Math.round(connectingText.height + cancelButton.height + 27), FlxColor.TRANSPARENT);
+		backdrop.drawRoundRect(1, 1, backdrop.width - 2, backdrop.height - 2, 20, 20, FlxColor.BLACK, {color: FlxColor.WHITE, thickness: 3});
+		backdrop.screenCenter();
+		for (item in [dcText, dcCaption, cancelButton])
+			item.screenCenter(X);
+
+		dcText.y = backdrop.y + 5;
+		dcCaption.y = dcText.y + dcText.height + 5;
+		cancelButton.y = dcCaption.y + dcCaption.height + 5;
+
+		for (item in [backdrop, dcText, dcCaption, cancelButton])
+		{
+			item.x = Math.round(item.x);
+			item.y = Math.round(item.y);
+			add(item);
+		}
+
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		_ap.poll();
+	}
+}
--- source/archipelago/APEntryState.hx
+++ source/archipelago/APEntryState.hx
@@ -260,7 +260,7 @@ class APEntryState extends FlxState
 				#else
 				var tags = ["AP", "Testing"];
 				#end
-				ap.ConnectSlot(_slotInput.text, _pwInput.text.length > 0 ? _pwInput.text : null, 0x7, tags, {major: 0, minor: 4, build: 3});
+				ap.ConnectSlot(_slotInput.text, _pwInput.text.length > 0 ? _pwInput.text : null, 0x7, tags, {major: 0, minor: 5, build: 0});
 			};
 
 			ap._hOnSlotRefused = (errors:Array<String>) ->
